0bbb56a4bb8688487ec9337bce6cfd9039473d79
Andalib Rahman Shagoto <andalibrahman369@gmail.com>
2024-05-14 20:30:43 +0600

    Ajax added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,23 +3,25 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>Laravel</title>
+        <title>AJAX CRUD</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
         
     </head>
     <body>
-        <table>
+        <table class="table-bordered">
             <tr>
                 <td>Id</td>
                 <td>Name</td>
                 <td>Email</td>
                 <td>Created At</td>
                 <td>Updated At</td>
+                <td>Action</td>
             </tr>
             @foreach(\App\Models\Student::all() as $user)
             <tr>
@@ -28,8 +30,14 @@
                 <td>{{ $user->email }}</td>
                 <td>{{ $user->created_at }}</td>
                 <td>{{ $user->updated_at }}</td>
+                <td>
+                    <button class="btn btn-success">Edit</button>
+                    <button class="btn btn-danger">Delete</button>
+                </td>
             </tr>
             @endforeach
         </table>
+
+       @include('script_file')
     </body>
 </html>
---
+++ resources/views/script_file.blade.php
@@ -0,0 +1,12 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script>
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+</script>
+<script>
+    // To be added
+</script>
